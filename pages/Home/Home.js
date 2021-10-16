90e7be8557b6680fdf3a99937f31b36aa7bd34a7
Jonathan Buitrago <47404028+JonathanB450@users.noreply.github.com>
2021-10-16 17:57:12 -0500

    Home and Store page

---
+++ pages/Home/Home.js
@@ -0,0 +1,26 @@
+Page({
+  data: {
+    searchBarInput: '',
+    store_items: [1,2,3,4,5,6,7,8,9,10]
+  },
+  onLoad() {},
+  onTap(){
+    console.log('clicked!')
+  },
+  onChange(e){
+    this.setData({
+      searchBarInput: e.detail.value
+    })
+    console.log(this.data.searchBarInput)
+  },
+  redirectToStore(){
+    my.navigateTo({
+      url: '/pages/Store/Store'
+    });
+  },
+  redirectToHome(){
+    my.navigateTo({
+      url: '/pages/Home/Home'
+    });
+  }
+});
