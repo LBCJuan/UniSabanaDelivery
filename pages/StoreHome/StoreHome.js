13c6870f0a942f6db36160fae1ef1d2fce1e6a22
Courel24 <susanaperez1993@hotmail.com>
2021-10-24 15:19:28 -0500

    StoreHomeFinal

--- pages/StoreHome/StoreHome.js
+++ pages/StoreHome/StoreHome.js
@@ -1,8 +1,12 @@
 Page({
   data: {   
-    items: [
+    recibido: [
         {id: 0, desc: 'Churrasco \nx1\nHamburguesa \nx2\nLimonada \nx1' }, 
         {id: 1,  desc: 'Torta de Chocolate\nx3\nCoca-Cola \nx2\nGranizado \nx1'},
+    ],
+    curso: [
+        {id: 0, desc: 'Coca-Cola \nx3\nJugo \nx1\nArepa de huevo \nx1' }, 
+        {id: 1,  desc: 'Torta de Zanahoria\nx1\nLimonada \nx2\nSandwich Vegetariano \nx1'},
     ]
   },
  
