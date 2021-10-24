838f5566f9b729546277560c698bbb526a73d332
Courel24 <susanaperez1993@hotmail.com>
2021-10-24 14:32:21 -0500

    HomeStore
    HomeStorex1

---
+++ pages/StoreHome/StoreHome.js
@@ -0,0 +1,9 @@
+Page({
+  data: {   
+    items: [
+        {id: 0, desc: 'Churrasco \nx1\nHamburguesa \nx2\nLimonada \nx1' }, 
+        {id: 1,  desc: 'Torta de Chocolate\nx3\nCoca-Cola \nx2\nGranizado \nx1'},
+    ]
+  },
+ 
+});
