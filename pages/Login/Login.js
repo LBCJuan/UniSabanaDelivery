311475e873f5ae1cdf31e6e50caeb345b662e977
Juan Carlos Hernandez Mariño <juanchobowser@gmail.com>
2021-10-24 15:36:26 -0500

    iidk
    ygvyg

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -12,36 +12,70 @@ Page({
   },
   redirectToHome() {
     if (this.data.clave && this.data.telefono) {
-      my.request({
-        url: `https://api-sabanadelivery.herokuapp.com/PerfilUsuario/verificar`,
-        headers: {},
-        method: 'POST',
-        data: {clave: this.data.clave, telefono: this.data.telefono},
-        timeout: 30000,
-        dataType: 'JSON',
-        success: (result) => {
-          console.log(result.data)
-          if (result.data[0]) {
-            my.redirectTo({
-              url: '/pages/Home/Home'
-            })
-          } else {
-            my.alert({ title: 'Oops', content: "Credenciales incorrectas"});
+      if (this.data.telefono[0] === "#") {
+        var codigo = this.data.telefono.replace("#", "");
+        my.request({
+          url: `https://api-sabanadelivery.herokuapp.com/PerfilTienda/login`,
+          headers: {},
+          method: 'POST',
+          data: { clave: this.data.clave, telefono: codigo },
+          timeout: 30000,
+          dataType: 'JSON',
+          success: (result) => {
+            console.log(result.data)
+            if (result.data[0]) {
+              my.redirectTo({
+                url: '/pages/StoreHome/StoreHome'
+              })
+            } else {
+              my.alert({ title: 'Oops', content: "Credenciales incorrectas" });
+              this.setData({
+                telefono: "",
+                clave: ""
+              })
+            }
+          },
+          fail: () => {
             this.setData({
-              telefono: "",
-              clave: ""
+              error: 'Ha ocurrido un error'
             })
-          }
-        },
-        fail: () => {
-          this.setData({
-            error: 'Ha ocurrido un error'
-          })
-        },
-        complete: () => {
+          },
+          complete: () => {
 
-        }
-      });
+          }
+        });
+      } else {
+        my.request({
+          url: `https://api-sabanadelivery.herokuapp.com/PerfilUsuario/verificar`,
+          headers: {},
+          method: 'POST',
+          data: { clave: this.data.clave, telefono: this.data.telefono },
+          timeout: 30000,
+          dataType: 'JSON',
+          success: (result) => {
+            console.log(result.data)
+            if (result.data[0]) {
+              my.redirectTo({
+                url: '/pages/Home/Home'
+              })
+            } else {
+              my.alert({ title: 'Oops', content: "Credenciales incorrectas" });
+              this.setData({
+                telefono: "",
+                clave: ""
+              })
+            }
+          },
+          fail: () => {
+            this.setData({
+              error: 'Ha ocurrido un error'
+            })
+          },
+          complete: () => {
+
+          }
+        });
+      }
     }
     else {
       my.alert({ title: 'Oops', content: 'Por favor completa todos los campos' });
