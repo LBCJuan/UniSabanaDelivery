db7db46bbd65025a0fea88e7b9057a1515fa6b12
Juan Carlos Hernandez Mariño <juanchobowser@gmail.com>
2021-10-24 09:01:05 -0500

    IDK

--- pages/Login/Login.js
+++ pages/Login/Login.js
@@ -3,19 +3,48 @@ Page({
     clave: '',
     telefono: '',
   },
-  onLoad() {},
-  redirectToHome(){
-    if(this.data.clave && this.data.telefono){
+  onLoad() { },
+  redirectToRegister() {
       my.redirectTo({
-        url: '/pages/Home/Home'
+        url: '/pages/Register/Register'
       })
-    }
-    else{
-      my.alert({title: 'Oops', content: 'Por favor completa todos los campos'});
-    }
-    
   },
-  updateInfo(e){
+  redirectToHome() {
+    if (this.data.clave && this.data.telefono) {
+      my.request({
+        url: `http://192.168.20.22:4700/PerfilUsuario/verificar`,
+        headers: {},
+        method: 'GET',
+        data: { clave: this.data.clave, telefono: this.data.telefono },
+        timeout: 30000,
+        dataType: '',
+        success: (result) => {
+          if (result)
+          var resultado = result;
+          my.alert({ title: 'Oops', content: resultado});
+          if (!isEmpyObject(resultado)){
+            my.redirectTo({
+              url: '/pages/Home/Home'
+            })
+          } else {
+            my.alert({ title: 'Oops', content: 'Error al iniciar sesión'});
+          }
+        },
+        fail: () => {
+          this.setData({
+            error: 'Ha ocurrido un error'
+          })
+        },
+        complete: () => {
+
+        }
+      });
+    }
+    else {
+      my.alert({ title: 'Oops', content: 'Por favor completa todos los campos' });
+    }
+  },
+  updateInfo(e) {
     this.setData({
       ...this.data,
       [e.target.dataset.id]: e.detail.value
