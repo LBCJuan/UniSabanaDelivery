b2730021f6259de9a146b94c8c153a57b4c4ec16
Juan Carlos Hernandez Mariño <juanchobowser@gmail.com>
2021-10-27 19:19:18 -0500

    Cart

--- .tea/entryFiles-development/index$.web.js
+++ .tea/entryFiles-development/index$.web.js
@@ -8,4 +8,3 @@ require('../../pages/Cart/Cart?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/History/History?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/Register/Register?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
 require('../../pages/StoreHome/StoreHome?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
-require('../../pages/CheckOut/CheckOut?hash=32d7d2807ed4e666ef03b4b3fe8c38ecf2e34e68');
