3fdbe45bf00327cb08eb3007e4e35ffd34e279e4
Juan Carlos Hernandez Mariño <juanchobowser@gmail.com>
2021-10-30 20:18:50 -0500

    StoreHome

--- pages/StoreHome/StoreHome.js
+++ pages/StoreHome/StoreHome.js
@@ -1,13 +1,74 @@
 Page({
-  data: {   
+  data: {
     recibido: [
-        {id: 0, desc: 'Churrasco \nx1\nHamburguesa \nx2\nLimonada \nx1' }, 
-        {id: 1,  desc: 'Torta de Chocolate\nx3\nCoca-Cola \nx2\nGranizado \nx1'},
+      { id: 0, desc: 'Churrasco \nx1\nHamburguesa \nx2\nLimonada \nx1' },
+      { id: 1, desc: 'Torta de Chocolate\nx3\nCoca-Cola \nx2\nGranizado \nx1' },
     ],
     curso: [
-        {id: 0, desc: 'Coca-Cola \nx3\nJugo \nx1\nArepa de huevo \nx1' }, 
-        {id: 1,  desc: 'Torta de Zanahoria\nx1\nLimonada \nx2\nSandwich Vegetariano \nx1'},
+      { id: 0, desc: 'Coca-Cola \nx3\nJugo \nx1\nArepa de huevo \nx1' },
+      { id: 1, desc: 'Torta de Zanahoria\nx1\nLimonada \nx2\nSandwich Vegetariano \nx1' },
     ]
   },
- 
+  onLoad() {
+    let codigoUsuario = my.getStorageSync({
+      key: "codigoUsuario"
+    })
+    this.setData({
+      codigoUsuario: codigoUsuario.data
+    })
+    this.traerVerificacion()
+    this.traerProceso()
+  },
+  group(xs, key) {
+    return xs.reduce(function (rv, x) {
+      (rv[x[key]] = rv[x[key]] || []).push(x);
+      return rv;
+    }, {});
+  },
+  traerVerificacion() {
+    my.request({
+      url: `https://api-sabanadelivery.herokuapp.com/DetalleCompra/pedidosEnVerificacion`,
+      headers: {},
+      method: 'POST',
+      data: { codigoUsuario: this.data.codigoUsuario },
+      timeout: 30000,
+      dataType: '',
+      success: (result) => {
+        this.setData({
+          recibido: Object.values(this.group(result.data, "codigoCompra"))
+        })
+      },
+      fail: () => {
+        this.setData({
+          error: 'Error getting orders'
+        })
+      },
+      complete: () => {
+
+      }
+    })
+  },
+  traerProceso() {
+    my.request({
+      url: `https://api-sabanadelivery.herokuapp.com/DetalleCompra/pedidosEnProceso`,
+      headers: {},
+      method: 'POST',
+      data: { codigoUsuario: this.data.codigoUsuario },
+      timeout: 30000,
+      dataType: '',
+      success: (result) => {
+        this.setData({
+          curso: Object.values(this.group(result.data, "codigoCompra"))
+        })
+      },
+      fail: () => {
+        this.setData({
+          error: 'Error getting orders'
+        })
+      },
+      complete: () => {
+
+      }
+    })
+  }
 });
